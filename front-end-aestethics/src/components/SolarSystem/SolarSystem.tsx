043296f61dbed47de7919e711e877db62c079ebf
Stolarek <przemyslaw.stolarek1@gmail.com>
2023-06-08 18:16:46 +0200

    implement visible orbits for planets

--- front-end-aestethics/src/components/SolarSystem/SolarSystem.tsx
+++ front-end-aestethics/src/components/SolarSystem/SolarSystem.tsx
@@ -9,8 +9,7 @@ import jupiter from '../../assets/SolarSystem/jupiter.png';
 import saturn from '../../assets/SolarSystem/saturn.png';
 import uranus from '../../assets/SolarSystem/uranus.png';
 import neptune from '../../assets/SolarSystem/neptune.png';
-import { useEffect } from 'react';
-
+import { useEffect, Fragment } from 'react';
 interface Planet {
   img: string;
   name: string;
@@ -96,7 +95,7 @@ export const SolarSystem = () => {
         }vmin`;
         currentPlanet.deg += currentPlanet.velocity / 750;
       });
-    }, 1 / 1000);
+    }, 1);
   }, []);
 
   return (
@@ -104,14 +103,23 @@ export const SolarSystem = () => {
       <img className='celestial' src={sun} alt='sun' id='sun' />
       {Object.entries(planets).map(([key, value]) => {
         return (
-          <>
+          <Fragment key={value.name}>
             <img
               className='celestial planet'
               src={value.img}
               alt={value.name}
               id={value.name}
             />
-          </>
+            <div
+              className='celestial'
+              style={{
+                border: 'solid white 1px',
+                opacity: 0.4,
+                background: 'transparent',
+                height: `${value.rad}vmin`,
+                width: `${value.rad}vmin`,
+              }}></div>
+          </Fragment>
         );
       })}
     </div>
